4ecd56d5158eed174deb2bfa4ff4a48b170ee446
seta1609 <sebastian.stp16@gmail.com>
2023-10-22 16:33:07 +0200

    first commit

---
+++ eAPI/src/test/java/dev/seta/eapi/EApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.seta.eapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
